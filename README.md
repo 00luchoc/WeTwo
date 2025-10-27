9fc0e46500ce9d6096068348dd52a7646f73c326
luciano Alcaraz <luciano28Alcaraz@gmail.com>
2025-10-27 09:37:09 -0300

    actualizacion de readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # WeTwo
+
+## TESTS BACK/FRONT
+
+### Back
+
+Para ejecutar los tests de backend primero se debe clonar el repo y acceder de la siguiente manera:
+
+`cd WeTwo-Back/test`
+
+Una vez dentro de la carpeta test, abrir la terminal y ejecutar el siguiente comando para ejecutarlos:
+
+`pytest -v`
--- WeTwo-Back/test/test_register.py
+++ WeTwo-Back/test/test_register.py
@@ -1,36 +1,13 @@
-import pytest
-from app import app
-
-def test_register_missing_fields(client):
-    """Prueba cuando faltan campos obligatorios."""
-    data = {"nombre": "Test"}
-    response = client.post("/register", json=data)
-    assert response.status_code == 400
-    assert b"Faltan campos obligatorios" in response.data
+import json
 
 def test_register_ok(monkeypatch, client):
-    """Simula registro exitoso sin conectar a la base de datos real."""
-
-    def mock_get_db_connection():
-        class MockConn:
-            def cursor(self): return self
-            def execute(self, query, params=None):
-                if "SELECT email" in query:
-                    self._fetchone = None
-            def fetchone(self): return self._fetchone
-            def close(self): pass
-            def commit(self): pass
-            # Simulación de is_connected()
-            def is_connected(self):
-                return True  # Simulamos que la conexión está activa
-        return MockConn()
-
-    monkeypatch.setattr("app.get_db_connection", mock_get_db_connection)
+    ...
     response = client.post("/register", json={
         "nombre": "Test User",
         "email": "test@example.com",
         "contraseña": "1234"
     })
     assert response.status_code == 201
-    assert "Usuario registrado con éxito" in response.data.decode('utf-8')
 
+    data = json.loads(response.data)
+    assert data["message"] == "Usuario registrado con éxito"
