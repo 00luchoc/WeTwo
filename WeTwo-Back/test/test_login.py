400444de46863116bd7d1e28e378e9f923e7af6c
Luciano Alcaraz <Luciano28Alcaraz@gmail.com>
2025-11-03 10:24:17 -0300

    Automatizacion del test de registro

--- WeTwo-Back/test/test_login.py
+++ WeTwo-Back/test/test_login.py
@@ -10,8 +10,8 @@ def client():
 def test_login_ok(client):
     """Prueba login exitoso usando la base de datos real de pruebas."""
     response = client.post("/login", json={
-        "email": "test@example.com",
-        "contraseña": "1234"
+        "email": "bruno@example.com",
+        "contraseña": "passbruno"
     })
     assert response.status_code == 200
     assert b"Login exitoso" in response.data
